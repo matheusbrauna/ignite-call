c4c641445569ff850835a6bc16ecb9a47c45a4be
matheusbrauna <mattsilvahenrique102@gmail.com>
2022-12-29 19:02:18 -0300

    feat: set user cookie to persist info

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 import { prisma } from '../../../lib/prisma'
 
 export default async function users(req: NextApiRequest, res: NextApiResponse) {
@@ -8,11 +9,28 @@ export default async function users(req: NextApiRequest, res: NextApiResponse) {
 
   const { name, username } = req.body
 
+  const usernameAlreadyExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (usernameAlreadyExists) {
+    return res.status(400).json({
+      message: 'Username already exists!',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@igniteCall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return res.status(201).json(user)
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { AxiosError } from 'axios'
 import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
@@ -48,6 +49,10 @@ export default function Register() {
         username,
       })
     } catch (err) {
+      if (err instanceof AxiosError && err.response?.data.message) {
+        return alert(err.response.data.message)
+      }
+
       console.log(err)
     }
   }

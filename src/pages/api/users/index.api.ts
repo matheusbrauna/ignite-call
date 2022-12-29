6af734086839fc23773e3fadb277f35e8c7fb25a
matheusbrauna <mattsilvahenrique102@gmail.com>
2022-12-29 18:44:26 -0300

    feat: add axios and create users route

---
+++ src/pages/api/users/index.api.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../lib/prisma'
+
+export default async function users(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { name, username } = req.body
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
+
+  return res.status(201).json(user)
+}
